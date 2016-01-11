c6d4fb3e2f94db2d26ba5564fbb88cc203022995
Maik Hummel <yo@beevelop.com>
2016-01-11 22:04:23 +0100

    Upgrade to v0.1.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,46 @@
 module.exports = function (grunt) {
-	grunt.initConfig({
+  grunt.initConfig({
 
-        clean: {
-            all: ['dist/']
-        },
+    clean: {
+      all: ['dist/']
+    },
 
-        concat: {
-            all: {
-                src: [
-                    'bower_components/angularjs/angular.js',
-                    'bower_components/sockjs/sockjs.js',
-                    'bower_components/stomp-websocket/lib/stomp.js',
-                    'bower_components/ng-stomp/ng-stomp.js'],
-                dest: 'dist/deps.min.js'
-            }
-        },
+    concat: {
+      all: {
+        src: [
+          'bower_components/angular/angular.js',
+          'bower_components/sockjs/sockjs.js',
+          'bower_components/stomp-websocket/lib/stomp.js',
+          'bower_components/ng-stomp/ng-stomp.js'],
+        dest: 'dist/deps.min.js'
+      }
+    },
 
-        processhtml: {
-            all: {
-                files: {
-                    'dist/index.html': ['src/index.html']
-                }
-            }
-        },
-
-		uglify: {
-			all: {
-				files: {
-					'dist/app.min.js': ['src/app.js'],
-                    //'dist/deps.min.js': ['dist/deps.min.js']
-				}
-			}
-		},
-        jshint: {
-            all: ['src/*.js']
+    processhtml: {
+      all: {
+        files: {
+          'dist/index.html': ['src/index.html']
         }
- 	});
+      }
+    },
 
- 	grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-processhtml');
+    uglify: {
+      all: {
+        files: {
+          'dist/app.min.js': ['src/app.js']
+        }
+      }
+    },
+    jshint: {
+      all: ['src/*.js']
+    }
+  });
 
-	grunt.registerTask('default', ['jshint', 'clean', 'concat', 'uglify', 'processhtml']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-processhtml');
+
+  grunt.registerTask('default', ['jshint', 'clean', 'concat', 'uglify', 'processhtml']);
 };
--- src/app.js
+++ src/app.js
@@ -1,47 +1,47 @@
 /**
  * STOMP-UI
  *
- * @version 0.0.1-alpha.1
- * @author Maik Hummel <yo@beevelop.com>
- * @license WTFPL
+ * @version 0.0.2
+ * @author Maik Hummel <m@ikhummel.com>
+ * @license MIT
  */
 
 angular
-    .module('stomp-ui', [
-        'ngStomp'
-    ])
-    .controller('AppCtrl', ['$stomp', '$scope', function ($stomp, $scope) {
+  .module('stomp-ui', [
+    'ngStomp'
+  ])
+  .controller('AppCtrl', ['$stomp', '$scope', function ($stomp, $scope) {
 
-        $scope.url = localStorage.getItem('url') || 'http://localhost:8080';
-        $scope.connectHeaders = localStorage.getItem('connectHeaders') || '{"login":"superadmin","passcode":"12345678"}';
-        $scope.client_destination = localStorage.getItem('client_destination') || '/rpc/';
-        $scope.payload = localStorage.getItem('payload') || '{"key":"value"}';
-        $scope.server_destination = localStorage.getItem('server_destination') || '/dest';
-        $scope.headers = localStorage.getItem('headers') || '{"rid": 1234}';
+      $scope.url = localStorage.getItem('url') || 'http://localhost:8080';
+      $scope.connectHeaders = localStorage.getItem('connectHeaders') || '{"login":"superadmin","passcode":"12345678"}';
+      $scope.client_destination = localStorage.getItem('client_destination') || '/rpc/';
+      $scope.payload = localStorage.getItem('payload') || '{"key":"value"}';
+      $scope.server_destination = localStorage.getItem('server_destination') || '/dest';
+      $scope.headers = localStorage.getItem('headers') || '{"rid": 1234}';
 
-        var logarea = document.getElementById('log');
-        $stomp.setDebug(function (args) {
-            document.getElementById('log').value += args + '\n-----------------------\n';
-            logarea.scrollTop = logarea.scrollHeight;
-        });
+      var logarea = document.getElementById('log');
+      $stomp.setDebug(function (args) {
+        document.getElementById('log').value += args + '\n-----------------------\n';
+        logarea.scrollTop = logarea.scrollHeight;
+      });
 
-        $scope.connect = function () {
-            localStorage.setItem('url', $scope.url);
-            localStorage.setItem('connectHeaders', $scope.connectHeaders);
+      $scope.connect = function () {
+        localStorage.setItem('url', $scope.url);
+        localStorage.setItem('connectHeaders', $scope.connectHeaders);
 
-            $stomp.connect($scope.url, JSON.parse($scope.connectHeaders));
-        };
+        $stomp.connect($scope.url, JSON.parse($scope.connectHeaders));
+      };
 
-        $scope.subscribe = function () {
-            localStorage.setItem('client_destination', $scope.client_destination);
-            $stomp.on($scope.client_destination);
-        };
+      $scope.subscribe = function () {
+        localStorage.setItem('client_destination', $scope.client_destination);
+        $stomp.on($scope.client_destination);
+      };
 
-        $scope.send = function () {
-            localStorage.setItem('server_destination', $scope.server_destination);
-            localStorage.setItem('payload', $scope.payload);
-            localStorage.setItem('headers', $scope.headers);
-            $stomp.send($scope.server_destination, JSON.parse($scope.payload), JSON.parse($scope.headers));
-        };
+      $scope.send = function () {
+        localStorage.setItem('server_destination', $scope.server_destination);
+        localStorage.setItem('payload', $scope.payload);
+        localStorage.setItem('headers', $scope.headers);
+        $stomp.send($scope.server_destination, JSON.parse($scope.payload), JSON.parse($scope.headers));
+      };
     }]
-);
+  );
